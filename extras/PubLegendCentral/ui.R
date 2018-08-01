526393120f61f066d8bb58276909f38f8720c322
schuemie <schuemie@ohdsi.org>
2018-08-01 14:43:48 -0700

    Making PubLegendCentral UI more like PMC

--- extras/PubLegendCentral/ui.R
+++ extras/PubLegendCentral/ui.R
@@ -1,16 +1,19 @@
 library(shiny)
 library(DT)
+source("widgets.R")
 
 shinyUI(fluidPage(style = 'width:1000px;',
-  titlePanel("PubLegend Central"),
-  verticalLayout(
-      textInput("search", label = "Search", placeholder = "Enter your search here"),
-      dataTableOutput("searchResults"),
-      conditionalPanel("output.rowIsSelected == true",
-                       uiOutput("abstract"),
-                       downloadLink("pdf", label = "PDF")
-      )
-  )
+                  titlePanel("PubLegend Central"),
+                  verticalLayout(
+                    div(style = "display:inline-block", textInput("query", label = "", placeholder = "Enter your search here")), 
+                    div(style = "display:inline-block", searchButton("searchButton", "Search", "query")),
+                    conditionalPanel("output.isSearchResultPage == true",
+                                     dataTableOutput("searchResults")),
+                    conditionalPanel("output.isAbstractPage == true",
+                                     uiOutput("abstract"),
+                                     downloadLink("pdf", label = "PDF")
+                    )
+                  )
 )
 )
 
