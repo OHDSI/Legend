723c066bcd2a025bc34f3f1322f7a86f4568c3fc
schuemie <schuemie@ohdsi.org>
2018-08-19 17:38:07 +0800

    Updated LEGEND data model. Drafted first version of protocol for depression.

---
+++ Documents/GenerateProtocol.R
@@ -0,0 +1,12 @@
+rmarkdown::render("Documents/ProtocolDepression.md",
+                  output_format = "html_document")
+
+rmarkdown::render("Documents/ProtocolDepression.md",
+                  output_format = "pdf_document")
+
+rmarkdown::render("Documents/ProtocolDepression.md",
+                  output_format = "word_document")
+
+
+rmarkdown::render("Documents/GuidingPrinciples.md",
+                  output_format = "word_document")
