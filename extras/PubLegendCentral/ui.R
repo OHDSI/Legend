5820cdc46135ceedc4ec881253f770644b6c153d
Marc A. Suchard <msuchard@ucla.edu>
2018-09-25 13:50:58 -0400

    add logo PLC

--- extras/PubLegendCentral/ui.R
+++ extras/PubLegendCentral/ui.R
@@ -3,7 +3,8 @@ library(DT)
 source("widgets.R")
 
 shinyUI(fluidPage(style = "width:1000px;",
-                  titlePanel("PubLegend Central"),
+                  titlePanel(title=div(img(src="logo.png", height = 50, width = 50), 
+                            "PubLegend Central")),
                   verticalLayout(div(style = "background-color: #AAAAAA;", tags$table(border = 0, width = "100%", tags$tr(tags$td(HTML("&nbsp;")), tags$td(textInput("query", label = "", placeholder = "Enter your search here", width = "100%")), tags$td(HTML("&nbsp;")), tags$td(align = "left", searchButton("searchButton", "Search", "query"))))),
                                  conditionalPanel("output.isSearchResultPage == true", dataTableOutput("searchResults")),
                                  conditionalPanel("output.isAbstractPage == true", 
