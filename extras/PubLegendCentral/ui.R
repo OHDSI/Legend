539ebe0a9a973966c4b4e9549fd810e1c30e9da7
Marc A. Suchard <msuchard@ucla.edu>
2018-09-27 14:02:13 -0400

    add favoriate icons and fix tab-panel name

--- extras/PubLegendCentral/ui.R
+++ extras/PubLegendCentral/ui.R
@@ -4,7 +4,8 @@ source("widgets.R")
 
 shinyUI(fluidPage(style = "width:1000px;",
                   titlePanel(title=div(img(src="logo.png", height = 50, width = 50), 
-                            "PubLegend Central")),
+                            "PubLegend Central"),
+                            windowTitle = "PubLegend Central"),
                   verticalLayout(div(style = "background-color: #AAAAAA;", tags$table(border = 0, width = "100%", tags$tr(tags$td(HTML("&nbsp;")), tags$td(textInput("query", label = "", placeholder = "Enter your search here", width = "100%")), tags$td(HTML("&nbsp;")), tags$td(align = "left", searchButton("searchButton", "Search", "query"))))),
                                  conditionalPanel("output.isSearchResultPage == true", dataTableOutput("searchResults")),
                                  conditionalPanel("output.isAbstractPage == true", 
